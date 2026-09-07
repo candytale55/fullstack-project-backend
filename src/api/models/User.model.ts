66035f4cb465442473009141fde00aaf3459f585
CandyTale55 <candytale55@gmail.com>
2026-09-07 10:48:17 +0200

    (User model) - Add TypeScript interface and schema formatting

--- src/api/models/User.model.ts
+++ src/api/models/User.model.ts
@@ -1,15 +1,42 @@
 import mongoose from "mongoose";
 
+// TypeScript interface for the User model.
+export interface IUser {
+    name: string;
+    email: string;
+    password: string;
+    role?: "user" | "admin";
+}
+
+
 // User schema definition for MongoDB using Mongoose.
 const userSchema = new mongoose.Schema({
-    name: { type: String, required: true, trim: true },
-    email: { type: String, required: true, trim: true, unique: true },
-    password: { type: String, required: true,  trim: true },
-    role: { type: String, enum: ["user", "admin"], default: "user" }
-}, { timestamps: true });
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        enum: ["user", "admin"],
+        default: "user"
+    }
+}, {
+    timestamps: true
+});
 
 // Create the User model based on the schema.
-const User = mongoose.model("User", userSchema, "users");
+const User = mongoose.model<IUser>("User", userSchema, "users");
 
 
 export default User;
